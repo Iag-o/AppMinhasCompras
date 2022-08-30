39a1062d0c6b60fe897c6e18b84b591ac83290a5
Usuario <Usuario@CPD03-M07>
2022-08-30 12:16:04 -0300

    Segunda atualização

--- AppMinhasCompras/AppMinhasCompras/Helper/SQLiteDatabaseHelper.cs
+++ AppMinhasCompras/AppMinhasCompras/Helper/SQLiteDatabaseHelper.cs
@@ -12,35 +12,37 @@ namespace AppMinhasCompras.Helper
 
         readonly SQLiteAsyncConnection _connection;
 
+
         public SQLiteDatabaseHelper(string path)
         {
             _connection = new SQLiteAsyncConnection(path);
             _connection.CreateTableAsync<Produto>().Wait();
         }
 
+
         public Task<int> Insert(Produto p)
         {
             return _connection.InsertAsync(p);
         }
 
+
         public void Update(Produto p)
         {
             string sql = "UPDATE Produto SET Descricao=?, Quantidade=?, Preco=?, WHERE Id=? ";
+
+            _connection.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
         }
 
-        public Task<Produto> GetById(int Id)
-        {
-            return new Produto();
-        }
 
         public Task<List<Produto>> GetAll()
         {
-
+            return _connection.Table<Produto>().ToListAsync();
         }
 
-        public void Delete(int Id)
-        {
 
+        public Task<int> Delete(int Id)
+        {
+           return _connection.Table<Produto>().DeleteAsync(i => i.Id == Id);
         }
     }
 
